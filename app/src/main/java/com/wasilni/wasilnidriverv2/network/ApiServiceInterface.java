291f21cbc1272fbd3006584db2fcf84f84b927bf
Mahmoud Mattar <mattarmahmoud96@gmail.com>
2019-04-06 15:24:38 +0300

    add get causes to api interfaces

--- app/src/main/java/com/wasilni/wasilnidriverv2/network/ApiServiceInterface.java
+++ app/src/main/java/com/wasilni/wasilnidriverv2/network/ApiServiceInterface.java
@@ -2,7 +2,9 @@ package com.wasilni.wasilnidriverv2.network;
 
 
 import com.wasilni.wasilnidriverv2.mvp.model.Booking;
+import com.wasilni.wasilnidriverv2.mvp.model.Cause;
 import com.wasilni.wasilnidriverv2.mvp.model.Ride;
+import com.wasilni.wasilnidriverv2.mvp.model.pojo.PaginationAPI;
 
 import java.util.List;
 
@@ -23,6 +25,10 @@ public interface ApiServiceInterface {
     @FormUrlEncoded
     @PUT("booking/{booking}/action")
     Call<Response<Booking>> ChangeState(@Header("Authorization") String Authorization , @Path("booking") int booking, @Field("status") String status);
+
+    @Headers("Accept: application/json")
+    @GET("captain_rating_cause")
+    Call<Response<PaginationAPI<Cause>>> GetCauses(@Header("Authorization") String Authorization  , @Query("perPage") int perPage);
 
     @Headers("Accept: application/json")
     @PUT("check")
