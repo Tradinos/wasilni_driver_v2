393a2e72cdbd043c67128500b930f4127797ef5d
Mahmoud Mattar <mattarmahmoud96@gmail.com>
2019-04-06 14:24:04 +0300

    add onoffdriver interface  presenter

---
+++ app/src/main/java/com/wasilni/wasilnidriverv2/mvp/view/OnOffDriverContract.java
@@ -0,0 +1,9 @@
+package com.wasilni.wasilnidriverv2.mvp.view;
+
+import com.wasilni.wasilnidriverv2.network.ServerPresenter;
+
+public interface OnOffDriverContract {
+    public interface OnOffDriverPresenter extends ServerPresenter<Boolean> {
+
+    }
+}
