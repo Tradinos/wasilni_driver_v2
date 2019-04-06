1a650fe4318788ea9c6d8f52f7daddfb04b9e409
Mahmoud Mattar <mattarmahmoud96@gmail.com>
2019-04-06 12:57:07 +0300

    add api

--- app/src/main/java/com/wasilni/wasilnidriverv2/network/ApiServiceInterface.java
+++ app/src/main/java/com/wasilni/wasilnidriverv2/network/ApiServiceInterface.java
@@ -1,6 +1,8 @@
 package com.wasilni.wasilnidriverv2.network;
 
 
+import com.wasilni.wasilnidriverv2.mvp.model.Ride;
+
 import java.util.List;
 
 import retrofit2.Call;
@@ -11,8 +13,14 @@ import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.Headers;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface ApiServiceInterface {
+    @Headers("Accept: application/json")
+    @FormUrlEncoded
+    @PUT("booking/{booking}/action")
+    Call<Response<Ride>> ChangeState(@Header("Authorization") String Authorization , @Path("booking") int booking, @Field("status") String status);
 
 }
