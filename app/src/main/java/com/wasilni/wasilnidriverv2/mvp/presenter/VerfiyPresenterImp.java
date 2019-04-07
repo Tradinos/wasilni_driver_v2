2143d1d06bf0b0ef9a5278e3de638b4a9de444c3
Mahmoud Mattar <mattarmahmoud96@gmail.com>
2019-04-07 14:04:13 +0300

    create mvp class imp

---
+++ app/src/main/java/com/wasilni/wasilnidriverv2/mvp/presenter/VerfiyPresenterImp.java
@@ -0,0 +1,23 @@
+package com.wasilni.wasilnidriverv2.mvp.presenter;
+
+import com.wasilni.wasilnidriverv2.mvp.view.VerifyContract;
+
+import retrofit2.Call;
+import retrofit2.Response;
+
+public class VerfiyPresenterImp implements VerifyContract.VerfiyPresenter {
+    @Override
+    public void sendToServer(Object request) {
+
+    }
+
+    @Override
+    public void onResponse(Call call, Response response) {
+
+    }
+
+    @Override
+    public void onFailure(Call call, Throwable t) {
+
+    }
+}
