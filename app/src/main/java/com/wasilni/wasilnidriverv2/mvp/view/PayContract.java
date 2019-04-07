c9d18acfd1fff56364a317899049d9b4a7501cd8
Mahmoud Mattar <mattarmahmoud96@gmail.com>
2019-04-07 10:59:06 +0300

    create pay mvp contract

---
+++ app/src/main/java/com/wasilni/wasilnidriverv2/mvp/view/PayContract.java
@@ -0,0 +1,10 @@
+package com.wasilni.wasilnidriverv2.mvp.view;
+
+import com.wasilni.wasilnidriverv2.network.Response;
+import com.wasilni.wasilnidriverv2.network.ServerPresenter;
+
+public interface PayContract {
+    public interface PayPresenter extends ServerPresenter {
+
+    }
+}
