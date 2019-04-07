fad4135109c1d463380b329a1f590bb80b1ecd0f
Mahmoud Mattar <mattarmahmoud96@gmail.com>
2019-04-07 14:40:51 +0300

    impl api verfiy and update user model

--- app/src/main/java/com/wasilni/wasilnidriverv2/mvp/presenter/CompleteDataPresenterImp.java
+++ app/src/main/java/com/wasilni/wasilnidriverv2/mvp/presenter/CompleteDataPresenterImp.java
@@ -1,24 +1,49 @@
 package com.wasilni.wasilnidriverv2.mvp.presenter;
 
+import android.util.Log;
+
 import com.wasilni.wasilnidriverv2.mvp.model.User;
 import com.wasilni.wasilnidriverv2.mvp.view.CompleteDataContract;
+import com.wasilni.wasilnidriverv2.network.ApiServiceInterface;
 import com.wasilni.wasilnidriverv2.network.Response;
+import com.wasilni.wasilnidriverv2.network.RetorfitSingelton;
 
 import retrofit2.Call;
 
 public class CompleteDataPresenterImp implements CompleteDataContract.CompleteDataPresenter {
     @Override
     public void sendToServer(User request) {
+        ApiServiceInterface service = RetorfitSingelton.getRetrofitInstance().create(ApiServiceInterface.class);
 
+        /** Call the method with parameter in the interface to get the notice data*/
+
+        Call<com.wasilni.wasilnidriverv2.network.Response<User>> call =
+                service.CompleteInfo( request);
+
+        call.enqueue(this);
     }
 
     @Override
     public void onResponse(Call<Response<User>> call, retrofit2.Response<Response<User>> response) {
+        Log.e("onResponse",response.message()+" code :"+response.code());
 
+        switch (response.code())
+        {
+            case 200 :
+                break;
+            case 422 :
+                break;
+            case 500 :
+                break;
+            case 400:
+                break;
+            case 401:
+                break;
+        }
     }
 
     @Override
     public void onFailure(Call<Response<User>> call, Throwable t) {
-
+        Log.e("onFailure",t.getMessage());
     }
 }
