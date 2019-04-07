a00e5daa8bc367fce95e26101f3f99bea21b5425
Mahmoud Mattar <mattarmahmoud96@gmail.com>
2019-04-07 14:03:33 +0300

    create mvp class imp

---
+++ app/src/main/java/com/wasilni/wasilnidriverv2/mvp/presenter/RequestActivitionCodePresentreImp.java
@@ -0,0 +1,23 @@
+package com.wasilni.wasilnidriverv2.mvp.presenter;
+
+import com.wasilni.wasilnidriverv2.mvp.view.RequestActivitionCodeContract;
+
+import retrofit2.Call;
+import retrofit2.Response;
+
+public class RequestActivitionCodePresentreImp implements RequestActivitionCodeContract.RequestActivitionCodePresentre {
+    @Override
+    public void sendToServer(Object request) {
+
+    }
+
+    @Override
+    public void onResponse(Call call, Response response) {
+
+    }
+
+    @Override
+    public void onFailure(Call call, Throwable t) {
+
+    }
+}
