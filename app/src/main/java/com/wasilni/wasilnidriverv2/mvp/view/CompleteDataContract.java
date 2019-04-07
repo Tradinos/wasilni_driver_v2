978acd372f594e6c933d9b80b7d02dc1434e3e71
Mahmoud Mattar <mattarmahmoud96@gmail.com>
2019-04-07 13:22:30 +0300

    create api mvp interface

--- app/src/main/java/com/wasilni/wasilnidriverv2/mvp/view/CompleteDataContract.java
+++ app/src/main/java/com/wasilni/wasilnidriverv2/mvp/view/CompleteDataContract.java
@@ -3,9 +3,8 @@ package com.wasilni.wasilnidriverv2.mvp.view;
 import com.wasilni.wasilnidriverv2.mvp.model.User;
 import com.wasilni.wasilnidriverv2.network.ServerPresenter;
 
-public interface RegistrationContract {
-    public interface RegistrationPresenter extends ServerPresenter<User> {
+public interface CompleteDataContract {
+    public interface CompleteDataPresenter extends ServerPresenter<User> {
 
     }
-
 }
--- app/src/main/java/com/wasilni/wasilnidriverv2/mvp/view/LoginContract.java
+++ app/src/main/java/com/wasilni/wasilnidriverv2/mvp/view/LoginContract.java
@@ -3,9 +3,8 @@ package com.wasilni.wasilnidriverv2.mvp.view;
 import com.wasilni.wasilnidriverv2.mvp.model.User;
 import com.wasilni.wasilnidriverv2.network.ServerPresenter;
 
-public interface RegistrationContract {
-    public interface RegistrationPresenter extends ServerPresenter<User> {
+public interface LoginContract {
+    public interface LoginPresenter extends ServerPresenter<User> {
 
     }
-
 }
