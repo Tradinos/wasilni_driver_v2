f274bff940855d9593ad16eaeba4ac59fb28dd3b
Mahmoud Mattar <mattarmahmoud96@gmail.com>
2019-04-08 15:17:30 +0300

    update ride model

--- app/src/main/java/com/wasilni/wasilnidriverv2/mvp/model/Ride.java
+++ app/src/main/java/com/wasilni/wasilnidriverv2/mvp/model/Ride.java
@@ -7,6 +7,8 @@ import java.util.List;
 
 public class Ride  {
     String status ;
+    String start_datetime ;
+    String passengaer_name;
     List<Booking> bookings ;
     public Ride(String status ) {
         this.status = status ;
@@ -26,5 +28,21 @@ public class Ride  {
 
     public void setBookings(List<Booking> bookings) {
         this.bookings = bookings;
+    }
+
+    public String getStart_datetime() {
+        return start_datetime;
+    }
+
+    public void setStart_datetime(String start_datetime) {
+        this.start_datetime = start_datetime;
+    }
+
+    public String getPassengaer_name() {
+        return passengaer_name;
+    }
+
+    public void setPassengaer_name(String passengaer_name) {
+        this.passengaer_name = passengaer_name;
     }
 }
