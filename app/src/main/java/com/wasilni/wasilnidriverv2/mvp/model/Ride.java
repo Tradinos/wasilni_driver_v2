be40b967420909371543f38dc0443f7071c6bbf8
Mahmoud Mattar <mattarmahmoud96@gmail.com>
2019-04-08 14:31:31 +0300

    update ride model

--- app/src/main/java/com/wasilni/wasilnidriverv2/mvp/model/Ride.java
+++ app/src/main/java/com/wasilni/wasilnidriverv2/mvp/model/Ride.java
@@ -3,9 +3,11 @@ package com.wasilni.wasilnidriverv2.mvp.model;
 
 import com.wasilni.wasilnidriverv2.util.RideStatus;
 
+import java.util.List;
+
 public class Ride  {
     String status ;
-
+    List<Booking> bookings ;
     public Ride(String status ) {
         this.status = status ;
     }
@@ -16,5 +18,13 @@ public class Ride  {
 
     public void setStatus(String status) {
         this.status = status;
+    }
+
+    public List<Booking> getBookings() {
+        return bookings;
+    }
+
+    public void setBookings(List<Booking> bookings) {
+        this.bookings = bookings;
     }
 }
