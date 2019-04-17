9824ca1495dd1f219ee69caa86b8f15fbaea4780
Mahmoud Mattar <mattarmahmoud96@gmail.com>
2019-04-17 14:59:37 +0300

    add max length

--- app/src/main/java/com/wasilni/wasilnidriverv2/ui/Activities/RegistrationActivity.java
+++ app/src/main/java/com/wasilni/wasilnidriverv2/ui/Activities/RegistrationActivity.java
@@ -60,7 +60,7 @@ public class RegistrationActivity extends FullScreenActivity implements
         Log.d("SAED", "initView: I am doing this for the greater good ");
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        Fragment phoneRegFragment = new InterviewRegistrationFragment();
+        Fragment phoneRegFragment = new PhoneRegistrationFragment();
         fragmentTransaction.add(R.id.content_frame,phoneRegFragment);
         fragmentTransaction.commit();
 
