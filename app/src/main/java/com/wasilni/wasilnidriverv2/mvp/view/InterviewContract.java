850308ba14000489aa02436b88b14038637d4816
Mahmoud Mattar <mattarmahmoud96@gmail.com>
2019-04-17 12:52:50 +0300

    add api interface

---
+++ app/src/main/java/com/wasilni/wasilnidriverv2/mvp/view/InterviewContract.java
@@ -0,0 +1,9 @@
+package com.wasilni.wasilnidriverv2.mvp.view;
+
+import com.wasilni.wasilnidriverv2.network.ServerPresenter;
+
+public interface InterviewContract {
+    public interface InterviewPresenter extends ServerPresenter<String , Object> {
+
+    }
+}
