29df8dc8416e45c4b8bc98dc67d791966edde820
Mahmoud Mattar <mattarmahmoud96@gmail.com>
2019-04-29 12:49:16 +0300

    delete car model

--- app/src/main/java/com/wasilni/wasilnidriverv2/mvp/model/Car.java
+++ app/src/main/java/com/wasilni/wasilnidriverv2/mvp/model/Car.java
@@ -23,5 +23,51 @@ public class Car {
     @Expose
     private String car_manufacture_year;
 
+    public Integer getColor_id() {
+        return color_id;
+    }
 
+    public void setColor_id(Integer color_id) {
+        this.color_id = color_id;
+    }
+
+    public Integer getBrand_id() {
+        return brand_id;
+    }
+
+    public void setBrand_id(Integer brand_id) {
+        this.brand_id = brand_id;
+    }
+
+    public Integer getBrand_model_id() {
+        return brand_model_id;
+    }
+
+    public void setBrand_model_id(Integer brand_model_id) {
+        this.brand_model_id = brand_model_id;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getInsurance_expired_date() {
+        return insurance_expired_date;
+    }
+
+    public void setInsurance_expired_date(String insurance_expired_date) {
+        this.insurance_expired_date = insurance_expired_date;
+    }
+
+    public String getCar_manufacture_year() {
+        return car_manufacture_year;
+    }
+
+    public void setCar_manufacture_year(String car_manufacture_year) {
+        this.car_manufacture_year = car_manufacture_year;
+    }
 }
