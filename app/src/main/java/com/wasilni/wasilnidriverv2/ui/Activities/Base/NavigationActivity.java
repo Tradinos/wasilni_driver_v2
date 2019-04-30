34c2eff39cd6a8b543fd5282863a1459bf6dae17
Mahmoud Mattar <mattarmahmoud96@gmail.com>
2019-04-30 12:30:50 +0300

    update side menu id

--- app/src/main/java/com/wasilni/wasilnidriverv2/ui/Activities/Base/NavigationActivity.java
+++ app/src/main/java/com/wasilni/wasilnidriverv2/ui/Activities/Base/NavigationActivity.java
@@ -69,17 +69,19 @@ public abstract class NavigationActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.profile) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.schedule) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.dailly) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.weekly) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.settings) {
+
+        } else if (id == R.id.notification) {
+
+        } else if (id == R.id.logout) {
 
         }
 
