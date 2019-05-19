c9dee7b0e82991eb426c8eb4eae8d9c7345cc8d9
Mahmoud Mattar <mattarmahmoud96@gmail.com>
2019-05-19 16:55:48 +0300

    add gps reciver and eventbus model

---
+++ app/src/main/java/com/wasilni/wasilnidriverv2/mvp/model/EventBusMessage.java
@@ -0,0 +1,27 @@
+package com.wasilni.wasilnidriverv2.mvp.model;
+
+public class EventBusMessage<T> {
+    private String ActivityName ;
+    private T data ;
+
+    public EventBusMessage(String activityName, T data) {
+        ActivityName = activityName;
+        this.data = data;
+    }
+
+    public String getActivityName() {
+        return ActivityName;
+    }
+
+    public void setActivityName(String activityName) {
+        ActivityName = activityName;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+}
